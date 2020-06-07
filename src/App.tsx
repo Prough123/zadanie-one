1366be21a419978a31722834d93abeddd8b47162
Илья Божок <slipok1999@mail.ru>
2020-06-07 18:53:04 +0300

    homeworkTwo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MessageContent from "./components/messageContent/messageContent";
+
+
+
+export type FilterValueType = "all" | "hight" | "low" | "middle";
 
 function App() {
 
     const Profile = [
-        {name: "Илья Божок", text:"Yarn top!", date:"12:00", avatar:"https://sun9-20.userapi.com/c824201/v824201969/173425/UZNGRozhtic.jpg?ava=1"},
+        {
+            name: "Илья Божок",
+            text: "Yarn top!",
+            date: "12:00",
+            avatar: "https://sun9-20.userapi.com/c824201/v824201969/173425/UZNGRozhtic.jpg?ava=1"
+        },
     ]
 
-    const Director = [
-        {name: "Сегрей Сипов", text:"БелГисс", date:"14:88", avatar:"https://sun1.beltelecom-by-minsk.userapi.com/OWvWE8LDS56yDyXJRaEBcqwyF_JNFmaT5Wre2w/P7opltbcJRc.jpg"},
-    ]
+
+    let [work, setWorking] = useState([
+        {id: 1, n: "работа", p: "hight"},
+        {id: 2, n: "аниме", p: "low"},
+        {id: 3, n: "игры", p: "middle"},
+        {id: 4, n: "реакт", p: "hight"},
+        {id: 5, n: "хтмл", p: "low"}
+    ])
+
+    let [filter, setFilter] = useState<FilterValueType>("all")
 
 
+    const removeDoings = (id: number) => {
+        let filteredWork = work.filter(t => t.id !== id);
+        setWorking(filteredWork)
+    }
+    let workForMessageContent = work;
+    const changeFilter = (value: FilterValueType) => {
+    setFilter(value)
+
+    }
+
+
+    //
+    if (filter !== "all") {
+        workForMessageContent = work.filter(work => work.p === filter)
+    }
+    // if (filter === "middle") {
+    //     workForMessageContent = work.filter(work => work.p === "middle")
+    // }
+    // if (filter === "hight") {
+    //     workForMessageContent = work.filter(work => work.p === "hight")
+    // }
 
     return (
         <div className="App">
-            <MessageContent content={Profile}/>
-            <MessageContent content={Director}/>
+            <MessageContent
+                content={Profile}
+                myWorking={workForMessageContent}
+                removeDoings={removeDoings}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
--- src/components/messageContent/messageContent.tsx
+++ src/components/messageContent/messageContent.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './messageContent.css'
-import MyQuality from "../myQualities/myQualities";
+import {FilterValueType} from "../../App";
 
-
-const Quality = ['html/pug ', 'css/scss ', 'bs4 ', 'бэм ', 'gulp ', 'html/pug ', 'css/scss ', 'bs4 ', 'бэм ', 'gulp ']
 
 type ContentProps = {
     name: string,
@@ -12,18 +10,56 @@ type ContentProps = {
     avatar: string,
 }
 
-type PropsType = {
-    content: Array<ContentProps>
+type myWorkingProps = {
+    id: number,
+    p: string,
+    n: string,
 }
+
+
+type PropsType = {
+    myWorking: Array<myWorkingProps>,
+    content: Array<ContentProps>,
+    removeDoings: (id: number) => void,
+    changeFilter: (value: FilterValueType) => void
+}
+
+
 const MessageContent = (props: PropsType) => {
+
+
+    let jsxMyWorking = props.myWorking.map((work) => {
+        return (
+            <li className='list-working' key={work.id}>
+                <span>{work.n}</span>
+                <button onClick={() => {
+                    props.removeDoings(work.id)
+                }}>remove
+                </button>
+            </li>
+        )
+    })
+
+
     return (
         <div className='wrapper'>
             <div className='avatar'><img width='40px' height='40px' src={props.content[0].avatar} alt=""/></div>
             <div className='box-content'>
                 <div className='container'>
                     <div className='name'>{props.content[0].name}</div>
-                    <MyQuality arrQuality={Quality}/>
+                    <ul className='list-working'>
+                        {jsxMyWorking}
+                    </ul>
                     <div className='date'>{props.content[0].date}</div>
+                </div>
+            </div>
+            <div>
+                <h1>Фильтры</h1>
+                <div>
+                    <button onClick={() => {props.changeFilter("all")}}>All</button>
+                    <button onClick={() => {props.changeFilter( "hight")}}>Hight</button>
+                    <button onClick={() => {props.changeFilter( "low")}}>Low</button>
+                    <button onClick={() => {props.changeFilter( "middle")}}>Middle</button>
                 </div>
             </div>
         </div>
